c3a1b7d7ce7933ec2cd184c641c601e7ff4f4b4f
gustavopcr <gustavopc2501@gmail.com>
2024-05-27 16:11:57 -0300

    update slice size when n bigger than slice length

--- internal/touchdown.go
+++ internal/touchdown.go
@@ -8,34 +8,6 @@ import (
 var count = make([]int, 20)
 var points = [...]int{3, 6, 7, 8}
 
-func max(nums ...int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-	max := nums[0]
-
-	for i := 0; i < len(nums); i++ {
-		if nums[i] > max {
-			max = nums[i]
-		}
-	}
-	return max
-}
-
-func min(nums ...int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-	min := nums[0]
-
-	for i := 0; i < len(nums); i++ {
-		if nums[i] < min {
-			min = nums[i]
-		}
-	}
-	return min
-}
-
 func countPos() {
 	for _, p := range points {
 		for i := 1; i < len(count); i++ {
@@ -52,6 +24,10 @@ func StartTouchdown() {
 }
 
 func GetPos(n int) int {
+	if(n>len(count)){
+		count = make([]int, n+1)
+		StartTouchdown()
+	}
 	return count[n]
 }
 
